fd486b161015eee7e02c83fba6897b550d4bb854
danielwilson90 <danielawilson90@gmail.com>
2026-05-29 22:25:17 -0700

    Show email address in contact section

--- script.js
+++ script.js
@@ -283,7 +283,7 @@ function contactPage() {
             behavioral insight can improve how people understand and use technology.
           </p>
           <div class="actions">
-            <a class="button primary" href="mailto:danielawilson90@gmail.com">Email Daniel</a>
+            <a class="button primary" href="mailto:danielawilson90@gmail.com">danielawilson90@gmail.com</a>
             <a class="button secondary" href="tel:+14153425232">(415) 342-5232</a>
             <a class="button secondary" href="#/work">View work</a>
           </div>
